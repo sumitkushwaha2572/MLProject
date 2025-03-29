1cb86f245fac41585a3378cbab8edc95676494ee
Sumit Kushwaha <sumitkushwaha2572@gmail.com>
2025-03-29 22:18:24 +0530

    model evaluating

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,16 +1,22 @@
-import os
-import sys
-from src.exception import CustomException
-from src.loggers import logging
-import pandas as pd
-from sklearn.model_selection import train_test_split
 # The dataclass module in Python provides a decorator (@dataclass) that simplifies the creation of classes for storing data. It automatically generates methods like __init__, __repr__, and __eq__, reducing boilerplate code.
 #Why Use @dataclass?
 #Automatic __init__ Method
 
 #No need to manually define an initializer; it’s auto-generated.
+import os
+import sys
+from src.exception import CustomException
+from src.loggers import logging
+import pandas as pd
 
+from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -47,8 +53,18 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e,sys)
+        
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
-        
-        
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
+
+
+
+
